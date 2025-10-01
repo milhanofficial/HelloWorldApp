3c19c8b0ebf666856985d38ac010465fed63e5fa
milhanofficial <milhan.official789@gmail.com>
2025-10-01 20:49:29 +0300

    "feat: added button that changes text on click"

--- app/src/main/java/com/example/helloworldapp/MainActivity.java
+++ app/src/main/java/com/example/helloworldapp/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.helloworldapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Step 5 & 6: Get references to Button and TextView
+        Button myButton = findViewById(R.id.myButton);
+        TextView myTextView = findViewById(R.id.myTextView);
+
+        // Set a click listener on the button
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to run when the button is clicked
+                myTextView.setText("The text has been changed!");
+            }
         });
     }
 }
